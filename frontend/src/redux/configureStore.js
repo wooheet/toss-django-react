55d4cdbe017fffcb91e8788c7713265d5866802e
haewon <30405705+wooheet@users.noreply.github.com>
2021-04-10 16:14:03 +0900

    Added reset css

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,21 +1,38 @@
 import { combineReducers, createStore, applyMiddleware } from "redux";
+import { routerReducer, routerMiddleware } from "react-router-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+// import createHistory from "history/createBrowserHistory";
 import thunk from "redux-thunk";
 import users from "redux/modules/users";
+import { i18nState } from "redux-i18n";
 
 const env = process.env.NODE_ENV;
 
-const middlewares = [thunk];
+const createHistory = require('history').createBrowserHistory;
+const history = createHistory();
 
-const reducer = combineReducers({
-  users
-});
+const middlewares = [thunk, routerMiddleware(history)];
 
 if (env === "development") {
   const { logger } = require("redux-logger");
   middlewares.push(logger);
 }
 
-let store = initialState =>
-  createStore(reducer, applyMiddleware(...middlewares));
+const reducer = combineReducers({
+  users,
+  routing: routerReducer,
+  i18nState
+});
+
+let store;
+
+if (env === "development") {
+  store = initialState =>
+    createStore(reducer, composeWithDevTools(applyMiddleware(...middlewares)));
+} else {
+  store = initialState => createStore(reducer, applyMiddleware(...middlewares));
+}
+
+export { history };
 
 export default store();
