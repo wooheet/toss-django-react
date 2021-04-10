5e39059e6f6dafa7a6b02ae778a28c8987917ee0
haewon <30405705+wooheet@users.noreply.github.com>
2021-04-10 16:32:32 +0900

    Change router to connected react router

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
 import styles from "./styles.scss";
-import Footer from "components/Footer";
 
 class App extends Component {
   render() {
     return (
-      <div >
-        {/*<Footer />*/}
+      <div className={styles.App}>
       </div>
     );
   }
