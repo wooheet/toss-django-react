b1b11913ebedb526e40a57ba78cacb1b7f691926
haewon <30405705+wooheet@users.noreply.github.com>
2021-04-10 16:52:25 +0900

    Added scss variables

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
-import styles from "./styles.scss";
+import styles from "./styles.module.scss";
+import Footer from "components/Footer";
 
 class App extends Component {
   render() {
     return (
       <div className={styles.App}>
+        <Footer />
       </div>
     );
   }
