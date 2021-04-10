15650627f922c91b57e6ac4450e76fff2fc1ffab
haewon <30405705+wooheet@users.noreply.github.com>
2021-04-10 16:52:25 +0900

    Added scss variables

---
+++ frontend/src/components/Footer/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import styles from "./styles.module.scss";
+import propTypes from "prop-types";
+
+class Footer extends React.Component {
+    static contextTypes = {
+        t: propTypes.func.isRequired
+    };
+    render() {
+        console.log(this.context)
+        return (
+            <footer className={styles.footer}>
+                <div className={styles.column}>
+                    <nav className={styles.nav}>
+                        <ul className={styles.list}>
+                            <li className={styles.listItem}>{this.context.t("About Us")}</li>
+                            <li className={styles.listItem}>Support</li>
+                            <li className={styles.listItem}>Blog</li>
+                            <li className={styles.listItem}>Press</li>
+                            <li className={styles.listItem}>API</li>
+                            <li className={styles.listItem}>Jobs</li>
+                            <li className={styles.listItem}>Privacy</li>
+                            <li className={styles.listItem}>Terms</li>
+                            <li className={styles.listItem}>Directory</li>
+                            <li className={styles.listItem}>Language</li>
+                        </ul>
+                    </nav>
+                </div>
+                <div className={styles.column}>
+                    <span className={styles.copyright}>© 2021 Toss</span>
+                </div>
+            </footer>
+        )
+    }
+}
+
+export default Footer;
